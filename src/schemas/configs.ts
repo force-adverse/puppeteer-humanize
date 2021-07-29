c9037d4ff5d1283bdc175224fb7239f04fe17445
prescience-data <presciencedata@gmail.com>
2021-07-29 21:33:19 +1000

    feat: define schemas

---
+++ src/schemas/configs.ts
@@ -0,0 +1,29 @@
+import { z } from "zod"
+
+export const DelaySchema = z.object({
+  chance: z.number().min(0).max(100).default(100),
+  min: z.number(),
+  max: z.number()
+})
+
+export const TypeIntoSchema = z.object({
+  mistakes: z
+    .object({
+      chance: z.number().default(5),
+      delay: DelaySchema.default({
+        min: 100,
+        max: 200
+      })
+    })
+    .default({}),
+  delays: z
+    .object({
+      all: DelaySchema.default({ chance: 100, min: 50, max: 150 }),
+      complete: DelaySchema.default({ chance: 100, min: 500, max: 1000 }),
+      space: DelaySchema.default({ chance: 80, min: 10, max: 100 }),
+      punctuation: DelaySchema.default({ chance: 70, min: 50, max: 500 }),
+      termination: DelaySchema.default({ chance: 95, min: 100, max: 1000 }),
+      cadence: DelaySchema.default({ chance: 100, min: 50, max: 500 })
+    })
+    .default({})
+})
